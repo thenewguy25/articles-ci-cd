d6feec4e9fec75af9faf26fcd7e1439c983dbcf6
John Nguyen <johnnguyen@johns-mbp-2.mynetworksettings.com>
2025-09-16 20:55:45 -0400

    Fix factory naming to match lowercase articles model

--- articles/database/factories/articlesFactory.php
+++ articles/database/factories/articlesFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\articles>
  */
-class ArticleFactory extends Factory
+class articlesFactory extends Factory
 {
     /**
      * Define the model's default state.
